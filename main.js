1b38d5c7a4094c13ab8a30788459e93777037271
Heropaster <heropasta@mail.ru>
2023-01-04 15:34:19 +0700

    create files, start work

---
+++ main.js
@@ -0,0 +1,14 @@
+const container = document.getElementById('itemsContainer')
+
+container.onmouseover = function() {
+    console.log(1)
+}
+container.onmouseout = function() {
+    console.log(2)
+}
+const renderItems = function(index) {
+
+}
+const getItems = function() {
+    
+}
