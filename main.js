6028c73d817b08684cf8f76641d3b39fee6f7f00
Heropaster <heropasta@mail.ru>
2023-01-04 20:39:50 +0700

    fetch connect

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 const container = document.getElementById('itemsContainer')
-
-container.onmouseover = function() {
-    console.log(1)
+//При наведении обратавыем таргетный элемент
+container.onclick = function() {
+    const currentItem = event.target
+    renderItems(currentItem)
 }
-container.onmouseout = function() {
-    console.log(2)
-}
-const renderItems = function(index) {
-
-}
-const getItems = function() {
+// container.onmouseout = function() {
+//     const prevItem = event.relatedTarget
     
+// }
+const renderItems = function(index = 0) {
+    if (index) {
+        for (let i = 0; i < index; i++) {
+            getItems(i)
+        }
+    }
+    else getItems()
+}
+const getItems = function(index = 0) {
+    return fetch('https://dummyjson.com/products')
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            container.innerHTML = `
+        <li>
+            ${(data.products[index].title)}
+        </li>
+    `
+        })
 }
