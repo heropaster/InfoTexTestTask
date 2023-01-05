eba808631622d52d0770978cf667b8f359af3a53
Heropaster <heropasta@mail.ru>
2023-01-05 19:29:19 +0700

    rework items rendering

--- main.js
+++ main.js
@@ -1,28 +1,42 @@
 const container = document.getElementById('itemsContainer')
-//При наведении обратавыем таргетный элемент
-container.onclick = function() {
-    const currentItem = event.target
-    renderItems()
-}
-// container.onmouseout = function() {
-//     const prevItem = event.relatedTarget
-    
-// }
-const renderItems = function(index = 10) {
-    if (index != 0) {
-        for (let i = 0; i < index; i++) {
-            getItems(i)
-        }
-    }
-}
-const getItems = function(index = 0) {
+//При загрузке страницы отрисовываются первые n элементов
+window.onload = function() {
     return fetch('https://dummyjson.com/products')
         .then((response) => {
             return response.json()
         })
         .then((data) => {
+            const products = data.products;
+            return renderItems(products)
+        })
+}
+//Вырисовка продуктов в необходимом количестве
+const renderItems = function(products) {
+    for (let i=0; i<10; i++) {
+        const itemId = products[i].id;
+        const title = products[i].title;
             container.innerHTML += `
-        <li class="item">
+        <li class="item" id=${itemId}>
+            ${(title)}
+        </li>
+    `
+    }
+}
+//Создание всплывающего окна
+container.onmouseover = function(event) {
+    let target = event.target;
+    
+}
+//Отображение продуктов на странице
+const getItems = function(index) {
+    return fetch('https://dummyjson.com/products')
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            const itemId = data.products[index].id
+            container.innerHTML += `
+        <li class="item" id=${itemId}>
             ${(data.products[index].title)}
         </li>
     `
