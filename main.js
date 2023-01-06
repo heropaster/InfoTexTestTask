0fadac4ba904e0bbb9a03f1399532b81ce218766
Heropaster <heropasta@mail.ru>
2023-01-06 16:31:51 +0700

    show tooltip on mouse hover

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const container = document.getElementById('itemsContainer')
+const container = document.getElementById('itemsContainer');
 let productsArr
 //При загрузке страницы отрисовываются первые n элементов
 window.onload = function() {
@@ -24,21 +24,45 @@ const renderItems = function(products, count=10) {
     `
     }
 }
- //Создание всплывающего окна
- container.onmouseover = function(event) {
-    //Определение элемента
+
+// let li = document.getElementById('itemsContainer').children;
+// console.log(li)
+container.onmouseover = function(event) {
     const target = event.target;
     if (target.className === "item") {
         const id = target.id;
         const itemObj = productsArr[id-1];
-    
-        let tooltipElem = document.createElement('div');
-        tooltipElem.className = 'tooltip';
+        
+        let tooltipElem = document.createElement("div");
+        document.body.append(tooltipElem)
+        tooltipElem.className = "tooltip";
         tooltipElem.id = id;
-    
+
         for (key in itemObj) {
-           tooltipElem.innerHTML += `${key}:${itemObj[key]}` 
+            if (key === 'id') {
+               tooltipElem.innerHTML += ""
+            }
+            else tooltipElem.innerHTML += `${key}:${itemObj[key]} `
         }
+        console.log(tooltipElem.style)
     }
-    
 }
+ //Создание всплывающего окна
+//  li.onmouseover = function(event) {
+//     console.log(1)
+//     //Определение элемента
+//     const target = event.target;
+//     if (target.className === "item") {
+//         const id = target.id;
+//         const itemObj = productsArr[id-1];
+    
+//         let tooltipElem = document.createElement('div');
+//         tooltipElem.className = 'tooltip';
+//         tooltipElem.id = id;
+    
+//         for (key in itemObj) {
+//            tooltipElem.innerHTML += `${key}:${itemObj[key]}` 
+//         }
+//     }
+    
+// }
